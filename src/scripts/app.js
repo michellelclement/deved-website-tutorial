535fe8e27e75716232916b360df3b445d1dd5be1
Michelle Clement <michellelclement@gmail.com>
2022-05-26 15:13:08 +0000

    Animate burger lines, open and close menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -57,8 +57,9 @@ function animateSlides() {
       .addTo(controller);
   });
 }
-let mouse = document.querySelector(".cursor");
-let mouseTxt = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 
 function cursor(e) {
   mouse.style.top = e.pageY + "px";
@@ -83,6 +84,24 @@ function activeCursor(e) {
   }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav", 1, { clipPath: "circle(2500px at 100% -10%)" });
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav", 1, { clipPath: "circle(50px at 100% -10%)" });
+  }
+}
+
+//Event listener
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 animateSlides();
