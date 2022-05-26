83f7813a4e5cfded0ff0d420ab14b6b86fa54ba0
Michelle Clement <michellelclement@gmail.com>
2022-05-26 15:14:32 +0000

    Remove scroll when menu is open

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -91,12 +91,14 @@ function navToggle(e) {
     gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
     gsap.to("#logo", 1, { color: "black" });
     gsap.to(".nav", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
   } else {
     e.target.classList.remove("active");
     gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
     gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
     gsap.to("#logo", 1, { color: "black" });
     gsap.to(".nav", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.add("hide");
   }
 }
 
