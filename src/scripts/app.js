ff37f8d0cb0adc0091138226c02a7baa6c24cdcf
Michelle Clement <michellelclement@gmail.com>
2022-05-26 10:31:57 +0000

    Add animation to nav

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -16,6 +16,8 @@ function animateSlides(){
         const slideTl = gsap.timeline({defaults: {duration: 1, ease: 'power2.inOut'}});
         slideTl.fromTo(revealImg, { x:'0%'}, { x:'100%'})
         slideTl.fromTo(img, {scale: 2}, {scale: 1}, '-=1'); // -= animates 1 second sooner
+        slideTl.fromTo(revealText, {x: '0%'}, {x: '100%'}, "-=0.74");
+        slideTl.fromTo(nav, {y: '-100%'}, {y:'0%'}, '-=0.5');
     });
 }
 
