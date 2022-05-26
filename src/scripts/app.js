a01c06c350aad8a54612ea58ac3b03dea1f5d0c0
Michelle Clement <michellelclement@gmail.com>
2022-05-26 14:44:03 +0000

    Fill in text animation when cursor hovers explore button on forest slide

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -74,10 +74,12 @@ function activeCursor(e) {
   }
   if (item.classList.contains("explore")) {
     mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, { y: "0%" });
     mouseTxt.innerText = "Tap";
   } else {
     mouse.classList.remove("explore-active");
     mouseTxt.innerText = "";
+    gsap.to(".title-swipe", 1, { y: "100%" });
   }
 }
 
