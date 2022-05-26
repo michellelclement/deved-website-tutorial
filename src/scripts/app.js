a91659cc206604099d3d565583c5088c5c5a4e47
Michelle Clement <michellelclement@gmail.com>
2022-05-26 14:15:03 +0000

    Cursor animation - circle enlarges over logo and burger

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -57,12 +57,23 @@ function animateSlides() {
       .addTo(controller);
   });
 }
+let mouse = document.querySelector(".cursor");
 
 function cursor(e) {
-  let mouse = document.querySelector(".cursor");
   mouse.style.top = e.pageY + "px";
   mouse.style.left = e.pageX + "px";
 }
-window.addEventListener("mousemove", cursor);
 
+function activeCursor(e) {
+  const item = e.target;
+  if(item.id === 'logo' || item.classList.contains('burger')){
+    mouse.classList.add('nav-active');
+  } else {
+    mouse.classList.remove('nav-active');
+  }
+}
+
+
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
 animateSlides();
